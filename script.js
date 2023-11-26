e802ef42193aaac5d6484b93c80ba73c9365df27
hnmiyd <tingweixian@gmail.com>
2023-11-26 22:21:05 +0800

    card and carousell, menu

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ const hamburger = document.getElementById("hamburger")
 const nav = document.querySelector(".menu")
 const header = document.querySelector("header")
 const links = document.querySelectorAll(".menu li")
+
+hamburger.addEventListener("click",()=>{
+    if(nav.classList.contains("nav-active")){
+        nav.classList.remove("")
+        header.classList.remove("")
+        hamburger.classList.add("")
+        hamburger.classList.remove("fa-x");
+    }
+    else{
+        nav.classList.add("nav-active")
+        header.classList.add("")
+        hamburger.classList.remove("")
+        hamburger.classList.add("fa-x");
+    }
+})
