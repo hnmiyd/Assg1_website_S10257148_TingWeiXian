1fe69c47d6f139b541cf10b35b755716b5d7e929
hnmiyd <tingweixian@gmail.com>
2023-11-26 22:15:46 +0800

    adding js and css

---
+++ script.js
@@ -0,0 +1,4 @@
+const hamburger = document.getElementById("hamburger")
+const nav = document.querySelector(".menu")
+const header = document.querySelector("header")
+const links = document.querySelectorAll(".menu li")
